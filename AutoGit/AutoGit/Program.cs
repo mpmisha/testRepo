f00fc708e34c52c596ca827cf7241c019362f246
Michael Merjanov <mimer@microsoft.com>
2016-07-07 20:40:37 +0300

    first commit

---
+++ AutoGit/AutoGit/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace AutoGit
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            RunAutoGit();
+        }
+
+        public static void RunAutoGit()
+        {
+            Process cmd = new Process();
+
+            cmd.StartInfo.FileName = "cmd.exe";
+            cmd.StartInfo.RedirectStandardInput = true;
+            cmd.StartInfo.RedirectStandardOutput = true;
+            cmd.StartInfo.CreateNoWindow = true;
+            cmd.StartInfo.UseShellExecute = false;
+
+            cmd.Start();
+            cmd.StandardInput.WriteLine("dir");
+            cmd.StandardInput.Flush();
+            cmd.StandardInput.Close();
+            var x = cmd.StandardOutput.ReadToEnd();
+            Console.WriteLine(x);
+        }
+    }
+}
---
+++ AutoGit/AutoGitTest/ProgramTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AutoGit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoGit.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+
+        [TestMethod()]
+        public void RunAutoGitTest()
+        {
+            AutoGit.Program.RunAutoGit();
+        }
+    }
+}
